dffc157535f367f76491a10ea6a16816069874d2
drmetik <romanos98d@gmail.com>
2020-01-15 09:59:47 +0200

    Google 1 hour timeout fix
    Fixed an issue where the google client would not operate after one hour. Implemented a background task for the bot that performs a relog every 60 minutes

--- Main.py
+++ Main.py
@@ -2,29 +2,31 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import discord
 import json
+from discord.ext import tasks
+import time
 
 # Google Spreadsheet connection
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("Credentials.json", scope)
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('Credentials.json', scope)
 google_client = gspread.authorize(credentials)
 
 spreadsheet_names = list()
-spreadsheet_names.append("Rolling System")
+spreadsheet_names.append('Rolling System')
 spreadsheet = google_client.open(spreadsheet_names[0])
-skills = spreadsheet.worksheet("Skills")
-xp = spreadsheet.worksheet("XP")
+skills = spreadsheet.worksheet('Skills')
+xp = spreadsheet.worksheet('XP')
 
 
 # Discord connection
-token = json.load(open("Credentials.json"))["discord_token"]
-client = discord.Client()
+token = json.load(open('Credentials.json'))['discord_token']
+discord_client = discord.Client()
 
 
-@client.event
+@discord_client.event
 async def on_message(message):
 
     channel = message.channel
-    if message.author == client.user:
+    if message.author == discord_client.user:
         return
 
     if message.content.startswith('!ping'):
@@ -40,11 +42,20 @@ async def on_message(message):
         if row_content == row_content2:
             await channel.send(row_content)
 
-@client.event
+
+@discord_client.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(discord_client.user.name)
+    print(discord_client.user.id)
     print('------')
+    google_login.start()
 
-client.run(token)
+
+@tasks.loop(minutes=60)
+async def google_login():
+    print("Relogging", time.asctime())
+    google_client.login()
+
+discord_client.run(token)
+
